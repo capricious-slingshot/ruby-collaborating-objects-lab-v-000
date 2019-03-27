bdcbbae761cc8e12dd1465ab8414ca34d32eb7ee
Carden Z <ephemere@protonmail.com>
2019-03-27 23:30:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,6 @@ class Song
   def new_by_filename(file)
     info = song.gsub(/.mp3/, '').split(' - ')
     Song.new(info[1], info[0], info[2])
-
   end
   # def artist=(artist_name)
   #   if self.artist.nil?
