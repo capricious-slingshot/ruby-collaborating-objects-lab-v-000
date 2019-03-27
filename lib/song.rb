c9f0d96940b860f2adc5f354a28c08ec8147fdcd
Carden Z <ephemere@protonmail.com>
2019-03-27 23:28:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
-  def initialize(name)
+  def initialize(name, artist=nil, genre= nil)
     @name = name
     @@all << self
   end
@@ -12,6 +12,11 @@ class Song
     @@all
   end
 
+  def new_by_filename(file)
+    info = song.gsub(/.mp3/, '').split(' - ')
+    Song.new
+
+  end
   # def artist=(artist_name)
   #   if self.artist.nil?
   #     self.artist = Artist.new(artist_name)
